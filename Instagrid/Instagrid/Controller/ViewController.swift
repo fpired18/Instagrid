5d1b4dad1315a9ebfcdedba7c35d695fd7dc27b2
fpired18 <fpired@wanadoo.fr>
2019-04-30 15:26:37 +0200

    stackView dans UIimage

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rectangleDessus: UIButton!
+    @IBOutlet weak var deuxCarresDessus: UIButton!
+    @IBOutlet weak var quatreCarres: UIButton!
+    @IBOutlet weak var imageContainer: ViewStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func rectangleAbove(_ sender: Any) {
+        
+        imageContainer.image1?.isHidden = true
+        imageContainer.image2?.isHidden = false
+        imageContainer.image3?.isHidden = false
+        imageContainer.image4?.isHidden = false
+    }
+    
+    @IBAction func twoSqaresAbove(_ sender: Any) {
+        imageContainer.image1?.isHidden = false
+        imageContainer.image2?.isHidden = false
+        imageContainer.image3?.isHidden = true
+        imageContainer.image4?.isHidden = false
+    }
+    
 
-
+    @IBAction func fourSqares(_ sender: Any) {
+        imageContainer.image1?.isHidden = false
+        imageContainer.image2?.isHidden = false
+        imageContainer.image3?.isHidden = false
+        imageContainer.image4?.isHidden = false
+    }
 }
 
