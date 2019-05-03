424069281741a383668e35c9e7ee1711240c8734
fpired18 <fpired@wanadoo.fr>
2019-05-03 10:01:50 +0200

    superpostion des vue de circulation

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -15,33 +15,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var quatreCarres: UIButton!
     @IBOutlet weak var imageContainer: ViewStackView!
     
+    @IBOutlet weak var left: UIImageView!
+    @IBOutlet weak var up: UIImageView!
+    
+    @IBOutlet weak var pictureValidation1: UIImageView!
+    @IBOutlet weak var pictureValidation2: UIImageView!
+    @IBOutlet weak var pictureValidation3: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        if UIDevice.current.orientation.isLandscape {
+            left.isHidden = false
+            up.isHidden = true
+        }
+        else {
+            left.isHidden = true
+            up.isHidden = false
+        }
     }
     
     
     @IBAction func rectangleAbove(_ sender: Any) {
         
         imageContainer.image1?.isHidden = true
-        imageContainer.image2?.isHidden = false
         imageContainer.image3?.isHidden = false
-        imageContainer.image4?.isHidden = false
+        pictureValidation1.isHidden = false
+        pictureValidation2.isHidden = true
+        pictureValidation3.isHidden = true
     }
     
     @IBAction func twoSqaresAbove(_ sender: Any) {
         imageContainer.image1?.isHidden = false
-        imageContainer.image2?.isHidden = false
         imageContainer.image3?.isHidden = true
-        imageContainer.image4?.isHidden = false
+        pictureValidation1.isHidden = true
+        pictureValidation2.isHidden = false
+        pictureValidation3.isHidden = true
     }
     
 
     @IBAction func fourSqares(_ sender: Any) {
         imageContainer.image1?.isHidden = false
-        imageContainer.image2?.isHidden = false
         imageContainer.image3?.isHidden = false
-        imageContainer.image4?.isHidden = false
+        pictureValidation1.isHidden = true
+        pictureValidation2.isHidden = true
+        pictureValidation3.isHidden = false
     }
 }
 
